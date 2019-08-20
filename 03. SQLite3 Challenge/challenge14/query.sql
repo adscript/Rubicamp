db9e38821372cbcf19ae46082677b7a97d457af6
adscript <adnanocode@gmail.com>
2019-08-20 14:25:29 +0700

    Nama Table jadi kontrak

--- 03. SQLite3 Challenge/challenge14/query.sql
+++ 03. SQLite3 Challenge/challenge14/query.sql
@@ -1,5 +1,5 @@
 CREATE TABLE mahasiswa(
-   nim    CHAR(8) PRIMARY KEY  NOT NULL,
+   nim    CHAR(8) PRIMARY KEY,
    nama           VARCHAR(20)  NOT NULL,
    alamat         VARCHAR(50)  NOT NULL,
    idJurusan      INTEGER,
@@ -12,7 +12,7 @@ CREATE TABLE jurusan(
 );
 
 CREATE TABLE dosen(
-   idDosen INTEGER PRIMARY KEY AUTOINCREMENT,
+   NIP         CHAR(12) PRIMARY KEY ,
    nama_dosen  VARCHAR(20)  NOT NULL
 );
 
@@ -22,28 +22,51 @@ CREATE TABLE matakuliah(
    sks        VARCHAR(30)  NOT NULL
 );
 
-CREATE TABLE nilai(
+CREATE TABLE kontrak(
    id INTEGER PRIMARY KEY AUTOINCREMENT,
-   nilai      VARCHAR(10), 
-   idMatkul   INTEGER,
+   nilai      CHAR(1) NOT NULL, 
+   idMatkul   INTEGER NOT NULL,
+   nim        CHAR(8) NOT NULL,
+   NIP    CHAR(12) NOT NULL,
    FOREIGN KEY(idMatkul) references matakuliah(idMatkul),
-   nim        CHAR(8),
    FOREIGN KEY(nim) references mahasiswa(nim),
-   idDosen    INTEGER,
-   FOREIGN KEY(idDosen) references dosen(idDosen)
+   FOREIGN KEY(NIP) references dosen(NIP)
 );
 
 INSERT INTO mahasiswa(nim,nama,alamat,idJurusan)
 VALUES ( '10115110', 'Adnan Radja Maulana', 'Ujungberung', 1 );
 
+INSERT INTO mahasiswa(nim,nama,alamat,idJurusan)
+VALUES ( '101151032', 'Romario', 'Bandung', 1 );
+
+INSERT INTO mahasiswa(nim,nama,alamat,idJurusan)
+VALUES ( '101151054', 'Rendy', 'Bandung', 1 );
+
 INSERT INTO jurusan(nama_jurusan)
 VALUES ('MATEMATIKA');
+INSERT INTO jurusan(nama_jurusan)
+VALUES ('MESIN');
+INSERT INTO jurusan(nama_jurusan)
+VALUES ('MINYAK');
 
 INSERT INTO dosen(nama_dosen)
-VALUES ('Warsoma');
+VALUES ('132049212301','Warsoma');
+INSERT INTO dosen(nama_dosen)
+VALUES ('132049212302','Denny');
+INSERT INTO dosen(nama_dosen)
+VALUES ('132049212303','Aditya');
+
 
 INSERT INTO matakuliah(nama_mk,sks)
 VALUES ('ANALISIS DATA', '4');
+INSERT INTO matakuliah(nama_mk,sks)
+VALUES ('TERMODINAMIKA', '4');
+INSERT INTO matakuliah(nama_mk,sks)
+VALUES ('AGAMA', '4');
 
-INSERT INTO nilai(nilai, idMatkul, nim, idDosen)
-VALUES ('90', 1, 10115110, 1);
+INSERT INTO kontrak(nilai, idMatkul, nim, idDosen)
+VALUES ('B', 1, '10115110', '132049212301');
+INSERT INTO kontrak(nilai, idMatkul, nim, idDosen)
+VALUES ('90', 1, '10115110', '132049212301');
+INSERT INTO kontrak(nilai, idMatkul, nim, idDosen)
+VALUES ('90', 1, '10115110', '132049212301');
