efd2e000bb33d6da63605b8b15cb8143433a42e8
adscript <adnanocode@gmail.com>
2019-08-22 17:05:21 +0700

    Mesin Hitung menggunakan export & import

---
+++ 04. Object Oriented Programming/challenge17/MesinHitung.js
@@ -0,0 +1,37 @@
+module.exports = class MesinHitung{
+    constructor(){
+        this.x = 1;
+    }
+    add(Num){
+        this.x += Num;
+        return this;
+    }
+    subtract(Num){
+        this.x -= Num;
+        return this;
+    }
+    divide(Num){
+        this.x = this.x / Num;
+        return this;
+    }
+    multiply(Num){
+        this.x *= Num;
+        return this;
+    }
+    square(){
+        this.x = Math.pow(this.x, 2);
+        return this;
+    }
+    exponent(Num){
+        this.x = Math.pow(this.x, Num);
+        return this;
+    }
+    squareRoot(){
+        this.x = Math.sqrt(this.x);
+        return this;
+    }
+    result(){
+        console.log(this.x);
+    }
+}
+
