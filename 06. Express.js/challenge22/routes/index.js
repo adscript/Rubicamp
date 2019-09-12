42f0167078d084a0cb071f76e6e054704648d0eb
adscript <adnanocode@gmail.com>
2019-09-12 19:28:56 +0700

    Fitur Add and Delete work

--- 06. Express.js/challenge22/routes/index.js
+++ 06. Express.js/challenge22/routes/index.js
@@ -8,7 +8,7 @@ moment().format();
 module.exports = (db) => {
 /* GET home page. */
   router.get('/', function(req, res, next) {
-    let arrKondisi = [ObjectId(req.query.valueID), req.query.valueString, parseInt(req.query.valueInt), parseFloat(req.query.valueFloat), req.query.valueBool];
+    let arrKondisi = [ObjectId(req.query.valueID), req.query.valueString, parseInt(req.query.valueInt), parseFloat(req.query.valueFloat), req.query.valueBool == 'true' ? true : false];
     let arrIsChecked = [req.query.isID, req.query.isString, req.query.isInt, req.query.isFloat, req.query.isBool];
     
     // let arrKondisi = [1, "coba", 2, 2.02, true];
@@ -26,15 +26,16 @@ module.exports = (db) => {
     }
     if(req.query.start && req.query.isDate){
       filter.date = {}
-      filter.date.$gte = req.query.start;
+      filter.date.$gte = new Date(`${req.query.start}T00:00:00.000Z`);
       if(req.query.end)
-        filter.date.$lte = req.query.end;
+        filter.date.$lte = new Date(`${req.query.end}T00:00:00.000Z`);
     }
+    console.log(filter);
     collection = db.collection("databaru");
+
     //START PAGINATION
     const limit = 3;
     const currpage = Number(req.query.page) || 1;
-    console.log(filter);
     url = req.query == '/' ? "?page=1" : req.query;
     collection.countDocuments(filter)
     .then(count => {
@@ -45,7 +46,6 @@ module.exports = (db) => {
       .skip(offset)
       .toArray()
       .then(result => {
-        console.log(result);
         res.render('index',{
                 data: result,
                 moment,
@@ -56,26 +56,30 @@ module.exports = (db) => {
           })
       }) 
     })
-
-    // collection.find(filter).countDocuments(count => {
-    //   const pages = Math.ceil(count/limit);
-    //   const offset = (currpage - 1) * limit;
-    //   collection.find(filter)
-    //   .limit(limit)
-    //   .skip(offset)
-    //   .toArray( result => {
-    //     console.log(result);
-    //     res.render('index',{
-    //       data: result,
-    //       moment,
-    //       query: req.query,
-    //       current: currpage,
-    //       pages : pages,
-    //       url : req.url
-    //     })
-    //   })
-    // })
   });
+
+  router.get('/add', (req, res) => res.render('add'));
+
+  router.post('/add',(req,res) => {
+    let insertValue = {"string": req.body.string, "integer": parseInt(req.body.integer), "float": parseFloat(req.body.float), "date": new Date(`${req.query.date}T00:00:00.000Z`), "bool": req.body.bool == 'true' ? true : false}
+    console.log(insertValue);
+    
+    db.collection("databaru")
+    .insertOne(insertValue)
+    .then(result => {
+      res.redirect('/');
+    })
+    .catch(err => console.log(err));
+  })
+
+  router.get('/delete/:id', (req, res) => {
+    const id = req.params.id;
+    db.collection("databaru")
+    .deleteOne({_id : ObjectId(id)})
+    .then(result => {
+      res.redirect('/');
+    })
+  })
 
   return router;
 }
